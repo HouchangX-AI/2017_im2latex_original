22a6057f0e3e501c46d26b508907ac3f8355c87c
JinSSSyu <37775638+JinSSSyu@users.noreply.github.com>
2020-03-05 10:42:19 +0800

    Update generator.py

--- src/generator.py
+++ src/generator.py
@@ -1,4 +1,6 @@
 import torch
+from queue import PriorityQueue
+import operator
 
 class GreedyDecoder:
     def __init__(self, model, max_len, hn, cn, on, encoder_outputs, device):
@@ -16,13 +18,11 @@ class GreedyDecoder:
         next_token = next_token.repeat(b) # (batch)
         logits = []
         preds = [next_token]
-        attn_weights = []
         hn = self.hn
         cn = self.cn
         on = self.on
         for t in range(1, self.max_len):
-            output, (hn, cn), on, attn_weight = self.model.decoder_step(next_token, hn, cn, on, self.encoder_outputs)
-            attn_weights.append(attn_weight.squeeze(1))
+            output, (hn, cn), on = self.model.decoder_step(next_token, hn, cn, on, self.encoder_outputs)
             logits.append(output)
             next_token = output.argmax(dim=1) # (batch)
             preds.append(next_token)
@@ -30,8 +30,7 @@ class GreedyDecoder:
         logits[0][:, start_token] = 1
         logits = torch.stack(logits, dim=1)
         preds = torch.stack(preds, dim=1)
-        attn_weights = torch.stack(attn_weights, dim=1)
-        return preds, logits, attn_weights
+        return preds, logits
 
 class BeamSearchDecoder:
     def __init__(self):
