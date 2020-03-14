ce45a3e38282810cb4f6431195681c8d033a4bd1
JinSSSyu <37775638+JinSSSyu@users.noreply.github.com>
2020-03-14 09:38:50 +0800

    Update generator.py

--- src_att/generator.py
+++ src_att/generator.py
@@ -33,3 +33,8 @@ class GreedyDecoder:
         attn_weights = torch.stack(attn_weights, dim=1)
         return preds, logits, attn_weights
 
+class BeamSearchDecoder:
+    def __init__(self):
+        pass
+    def generate(self):
+        pass
